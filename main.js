387a34826362a9422d571966ff0aac7c73ad23b3
Modemaker <uveraw@gmail.com>
2025-01-11 19:06:12 +0900

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-function toggle() {
-  const Element = document.getElementsByClassName("profile-list")[0]
-
-  if( Element.classList.length == 1 ) {
-    Element.className += " hidden"
-  } else if( Element.classList.length == 2 ) {
-    Element.className = "profile-list"
-  }
-}
